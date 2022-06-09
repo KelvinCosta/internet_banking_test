712e1c65b97b3fd27836430662116c40e975d7c0
Kelvin Costa <kelvin.costa@capgemini.com>
2022-06-09 17:46:21 -0300

    Adding unit test
    Adding BankingController test cases to be implement

--- src/test/java/com/example/demo/BankingControllerTest.java
+++ src/test/java/com/example/demo/BankingControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,13 +17,53 @@ import org.springframework.test.web.servlet.MockMvc;
 public class BankingControllerTest {
 
 	private static final String BASE_URI = "/banking";
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Test
 	void canary() {
 		assertTrue(true);
 	}
-	
+
+	@Test
+	void testCadastrarCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetornarTodosClientes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSacarValorMenorQue100() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSacarValorEntre100E300() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSacarValorMaiorQue300() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSacarValorPlanoExclusive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDepositar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsultarHistoricoTransacoes() {
+		fail("Not yet implemented");
+	}
+
 }
