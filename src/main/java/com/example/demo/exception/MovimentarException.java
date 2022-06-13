91bbc27033b177371a5c1fbdefb292e28c245033
Kelvin Costa <kelvin.costa@capgemini.com>
2022-06-13 20:41:49 -0300

    Update MovimentarException.java
    Renamed from SacarException to MovimentarException

--- src/main/java/com/example/demo/exception/MovimentarException.java
+++ src/main/java/com/example/demo/exception/MovimentarException.java
@@ -3,11 +3,11 @@ package com.example.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
-public class SacarException extends HttpClientErrorException {
+public class MovimentarException extends HttpClientErrorException {
 
 	private static final long serialVersionUID = -1945288925948484513L;
 
-	public SacarException(HttpStatus statusCode, String statusText) {
+	public MovimentarException(HttpStatus statusCode, String statusText) {
 		super(statusCode, statusText);
 	}
 }
